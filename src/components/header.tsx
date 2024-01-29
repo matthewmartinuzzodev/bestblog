279bd39e022abcb9667000fe40d28803a0370e7b
RMITmatthewmartinuzzo <s3850470@student.rmit.edu.au>
2024-01-30 08:11:30 +1100

    Fix imports

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,3 @@
-import Link from "next/link"
-import { Icon } from '@iconify-icon/react';
-
 export const Header = () => {
     return (
         <div className="flex items-center justify-end h-24 bg-slate-100">
